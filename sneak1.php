7f19c172be7b8a1b5f13e175ce930f24a4f81dde
daniele-pipino <Danielemacpipino@icloud.com>
2021-09-23 13:48:48 +0200

    sneak 1 initial commit

---
+++ sneak1.php
@@ -0,0 +1,8 @@
+<?php 
+
+/*
+
+SNACK 1
+Creare una funzione che restituisce un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta. Stampare i numeri in pagina.
+
+*/
